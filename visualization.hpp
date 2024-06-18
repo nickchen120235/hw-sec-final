15188b2efd5e50be48a4a1439bc9857b10157045
Ander <weiwei061689@gmail.com>
2024-06-19 00:50:04 +0800

    Remove intermediate gate

--- visualization.hpp
+++ visualization.hpp
@@ -5,6 +5,56 @@
 
 namespace Visualization {
 
+std::string get_node_expression(const core::Node* node) {
+
+  if (node->type == core::GateType::INPUT) {
+    // std::cout << node->name << ": " << node->name << std::endl;
+    return std::string(node->name);
+  }
+
+  if (node->type == core::GateType::NOT) {
+    // std::cout << node->inputs[0]->name << ": " << std::string("!(" + node->inputs[0]->name + ")") << std::endl;
+    return std::string("!(" + get_node_expression(node->inputs[0]) + ")");
+  }
+
+  if (node->type == core::GateType::BUF) {
+    std::cout << node->inputs[0]->name << ": " << std::string("!(" + node->inputs[0]->name + ")") << std::endl;
+    return std::string(get_node_expression(node->inputs[0]));
+  }
+
+  std::string output = "";
+  std::string prefix = "(";
+
+  if (node->type == core::GateType::NOR || node->type == core::GateType::NAND || node->type == core::GateType::XNOR) {
+    prefix = "!(";
+  }
+
+  output = get_node_expression(node->inputs[0]);
+
+  for (std::size_t i = 1; i < node->inputs.size(); ++i) {
+
+    output = prefix + output;
+
+    if (node->type == core::GateType::NOR || node->type == core::GateType::OR) {
+      output += " | ";
+    }
+    else if (node->type == core::GateType::NAND || node->type == core::GateType::AND) {
+      output += " & ";
+    }
+    else if (node->type == core::GateType::XOR || node->type == core::GateType::XNOR) {
+      output += " ^ ";
+    }
+    else {
+      std::cout << "Someting went wrong" << std::endl;
+    }
+
+    output += get_node_expression(node->inputs[i]) + ")";
+  }
+
+  // std::cout << node->name << ": " << output << std::endl;
+  return output;
+}
+
 void write_to_verilog_file(const core::NodeMap& node_map) {
 
   std::ofstream file("output.v");
@@ -13,7 +63,7 @@ void write_to_verilog_file(const core::NodeMap& node_map) {
     exit(1);
   }
 
-  file << "module output_module(";
+  file << "module top(";
 
   for (std::size_t i = 0; i < node_map.inputs.size(); ++i) {
     file << node_map.inputs[i]->name;
@@ -59,59 +109,8 @@ void write_to_verilog_file(const core::NodeMap& node_map) {
 
   file << ";\n\n";
 
-  file << "wire ";
-
-  for (std::size_t i = 0; i < node_map.gates.size(); ++i) {
-    file << node_map.gates[i]->name;
-    if (i < node_map.gates.size() - 1) {
-      file << ',';
-    }
-  }
-
-  file << ";\n\n";
-
-  for (const auto& node : node_map.gates) {
-    switch (node->type) {
-#define _(x, y, z, w)                                                                                                  \
-  case core::GateType::y:                                                                                              \
-    file << w;                                                                                                         \
-    break;
-      foreach_gate_type_no_in_out
-#undef _
-    }
-
-    file << "(" << node->name << ",";
-
-    for (std::size_t i = 0; i < node->inputs.size(); ++i) {
-      file << node->inputs[i]->name;
-      if (i < node->inputs.size() - 1) {
-        file << ",";
-      }
-    }
-
-    file << ");" << std::endl;
-  }
-
-  for (const auto& node : node_map.out_gates) {
-    switch (node->type) {
-#define _(x, y, z, w)                                                                                                  \
-  case core::GateType::y:                                                                                              \
-    file << w;                                                                                                         \
-    break;
-      foreach_gate_type_no_in_out
-#undef _
-    }
-
-    file << "(" << node->name << ",";
-
-    for (std::size_t i = 0; i < node->inputs.size(); ++i) {
-      file << node->inputs[i]->name;
-      if (i < node->inputs.size() - 1) {
-        file << ",";
-      }
-    }
-
-    file << ");" << std::endl;
+  for (core::Node* node : node_map.outputs) {
+    file << "assign " << node->name << " = " << get_node_expression(node) << ";\n";
   }
 
   file << "\nendmodule";
