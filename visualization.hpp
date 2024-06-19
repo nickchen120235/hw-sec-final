ee8057d8ef941a6e1b4474628b5cfb74e53073d1
Ander <weiwei061689@gmail.com>
2024-06-19 10:17:01 +0800

    Add comment

--- visualization.hpp
+++ visualization.hpp
@@ -8,22 +8,26 @@ namespace Visualization {
 
 std::string get_node_expression(std::unordered_map<const core::Node*, std::string>& dp, const core::Node* node) {
 
+  // if node is already visited, return th expression
   if (dp.find(node) != dp.end()) {
     return dp[node];
   }
 
+  // gate is input, return itself
   if (node->type == core::GateType::INPUT) {
     // std::cout << node->name << ": " << node->name << std::endl;
     dp[node] = std::string(node->name);
     return std::string(node->name);
   }
 
+  // return !(gate)
   if (node->type == core::GateType::NOT) {
     // std::cout << node->inputs[0]->name << ": " << std::string("!(" + node->inputs[0]->name + ")") << std::endl;
     dp[node] = std::string("!(" + get_node_expression(dp, node->inputs[0]) + ")");
     return std::string("!(" + get_node_expression(dp, node->inputs[0]) + ")");
   }
 
+  // return (gate)
   if (node->type == core::GateType::BUF) {
     // std::cout << node->inputs[0]->name << ": " << std::string("!(" + node->inputs[0]->name + ")") << std::endl;
     dp[node] = std::string(get_node_expression(dp, node->inputs[0]));
@@ -33,16 +37,20 @@ std::string get_node_expression(std::unordered_map<const core::Node*, std::strin
   std::string output = "";
   std::string prefix = "(";
 
+  // if gate is a N__ gate, use prefix "!(" gate)
   if (node->type == core::GateType::NOR || node->type == core::GateType::NAND || node->type == core::GateType::XNOR) {
     prefix = "!(";
   }
 
+  // [input0]
   output = get_node_expression(dp, node->inputs[0]);
 
   for (std::size_t i = 1; i < node->inputs.size(); ++i) {
 
+    // !([input0]
     output = prefix + output;
 
+    // add operator
     if (node->type == core::GateType::NOR || node->type == core::GateType::OR) {
       output += " | ";
     }
@@ -53,9 +61,11 @@ std::string get_node_expression(std::unordered_map<const core::Node*, std::strin
       output += " ^ ";
     }
     else {
-      std::cout << "Someting went wrong" << std::endl;
+      // node should only be this six gate types.
+      std::cout << "Something went wrong" << std::endl;
     }
 
+    // !([input0] [operator] [input1])
     output += get_node_expression(dp, node->inputs[i]) + ")";
   }
 
@@ -66,12 +76,14 @@ std::string get_node_expression(std::unordered_map<const core::Node*, std::strin
 
 void write_to_verilog_file(const core::NodeMap& node_map, bool show_intermidiate_gate = false) {
 
+  // write to output.v
   std::ofstream file("output.v");
 
   if (!file.is_open()) {
     exit(1);
   }
 
+  // write module header
   file << "module top(";
 
   for (std::size_t i = 0; i < node_map.inputs.size(); ++i) {
@@ -96,6 +108,8 @@ void write_to_verilog_file(const core::NodeMap& node_map, bool show_intermidiate
 
   file << "\n\n";
 
+
+  // write input gates
   file << "input ";
 
   for (std::size_t i = 0; i < node_map.inputs.size(); ++i) {
@@ -107,6 +121,8 @@ void write_to_verilog_file(const core::NodeMap& node_map, bool show_intermidiate
 
   file << ";\n\n";
 
+
+  // write output gates
   file << "output ";
 
   for (std::size_t i = 0; i < node_map.outputs.size(); ++i) {
@@ -120,6 +136,9 @@ void write_to_verilog_file(const core::NodeMap& node_map, bool show_intermidiate
 
   if (show_intermidiate_gate) {
 
+    // don't reduce intermidiate gates
+
+    // write intermidiate gates
     for (const auto& node : node_map.gates) {
       switch (node->type) {
 #define _(x, y, z, w)                                                                                                  \
@@ -142,6 +161,7 @@ void write_to_verilog_file(const core::NodeMap& node_map, bool show_intermidiate
       file << ");" << std::endl;
     }
 
+    // write output gates
     for (const auto& node : node_map.outputs) {
       switch (node->type) {
 #define _(x, y, z, w)                                                                                                  \
@@ -168,6 +188,7 @@ void write_to_verilog_file(const core::NodeMap& node_map, bool show_intermidiate
 
     std::unordered_map<const core::Node*, std::string> dp_map;
 
+    // write gate expression from output gates
     for (core::Node* node : node_map.outputs) {
       file << "assign " << node->name << " = " << get_node_expression(dp_map, node) << ";\n";
     }
